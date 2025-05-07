1f0fd36d3b75c97c9a3f3a212765ee70597e3668
VedantGadge <vedant.gadgegsis@gmail.com>
2025-05-07 12:02:08 +0530

    .

--- q6/findAllUsers.js
+++ q6/findAllUsers.js
@@ -1,13 +1,28 @@
-const mongoose = require('mongoose')
-const user = require('../q5/schema')
+const mongoose = require("mongoose");
+const user = require("../q5/schema");
+const express = require("express");
 
-mongoose.connect('mongodb://localhost:27017/userDatabase').then(()=>{
-    console.log('Mongoose connected')
-    return user.find({})
-}).then((data)=>{
-    console.log('All users from the databse: \n',data)
-    mongoose.connection.close()
-}).catch((err)=>{
-    console.log("Error",err)
-    mongoose.connection.close()
-})
+const app = express();
+const PORT = 3000;
+
+mongoose
+  .connect("mongodb://localhost:27017/userDatabase")
+  .then(() => {
+    console.log("Mongoose connected");
+  })
+  .catch((err) => {
+    console.log("Error", err);
+  });
+
+app.get("/", async (req, res) => {
+  try {
+    const users = await user.find({});
+    res.send(`<h1>All users</h1><pre>${JSON.stringify(users, null, 2)}</pre>`); //JSON.stringify converts JS obj/arrays into string , null means no filter fn applied, 2 is no of spaced for indentation
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
